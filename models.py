8a6aea8b704c45f592a2a11da91526c60e4c7d69
marianaakuma <153663415+marianaakuma@users.noreply.github.com>
2025-05-26 23:17:44 +0000

    26/05/25

--- models.py
+++
@@ -1,7 +0,0 @@
-from db import db
-
-class usuario(db.model):
-    __tablename__ = 'usuario'
-    id = db.column(db.interger,primary_key= True)
-    nome = db.column(db.string(30), unique=True)
-    senha = db.column(db.string())
